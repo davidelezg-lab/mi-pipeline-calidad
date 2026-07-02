a30411c74049a351e565badf5ca863dcdf64d529
davidelezg-lab <davidelezg@gmail.com>
2026-07-03 00:11:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,17 @@
 #include <iostream>
+#include <cstdio>
 
 int suma(int a, int b) {
     return a + b;
 }
 
-int resta(int a, int b) {
-    return a - b;
-}
-
-int multiplicar(int a, int b) {
-    return a * b;
-}
-
-int dividir(int a, int b) {
-
-    if (b == 0) {
-        return 0;
-    }
-
-    return a / b;
-}
-
 int main() {
 
-    std::cout << suma(10,20) << std::endl;
+    char buffer[10];
 
-    std::cout << resta(20,10) << std::endl;
+    gets(buffer);
 
-    std::cout << multiplicar(3,4) << std::endl;
-
-    std::cout << dividir(20,4) << std::endl;
-
-    std::cout << dividir(10,0) << std::endl;
+    std::cout << buffer << std::endl;
 
     return 0;
 }
