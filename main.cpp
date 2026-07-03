cd6be52520561c8b24b2a2ab88d3fb992ca50122
davidelezg-lab <davidelezg@gmail.com>
2026-07-03 08:04:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
-#include <cstdio>
+
+int suma(int a, int b) {
+    return a + b;
+}
+
+int resta(int a, int b) {
+    return a - b;
+}
+
+/*
+ * ERROR INTENCIONADO
+ * Debería ser:
+ * return a * b;
+ */
+int multiplicar(int a, int b) {
+    return a + b;
+}
+
+int dividir(int a, int b) {
+
+    if (b == 0) {
+        return 0;
+    }
+
+    return a / b;
+}
 
 int main() {
 
-    char buffer[10];
-
-    fgets(buffer, sizeof(buffer), stdin);
-
-    std::cout << buffer << std::endl;
+    std::cout << suma(10, 20) << std::endl;
 
     return 0;
-}
-
-int multiplicar(int a, int b) {
-    return a + b;
 }
 
